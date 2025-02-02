6e5eb20ea03c316ed7d7e940cc94cebe23d4c38f
KangSandeul <deul101@daum.net>
2025-02-02 12:47:12 +0900

    'resevedList _ 예약상세보기'

--- com.sd.hotel/src/main/resources/static/js/reservedList.js
+++ com.sd.hotel/src/main/resources/static/js/reservedList.js
@@ -1,58 +1,60 @@
-$(document).ready(function() {
-
-
 window.addEventListener('load', () => {
-  
-  
-    fetch('/admin/reservation/roomList.do',{
-      method:'POST',
-      headers:{
-        'Content-Type' : 'application/json'
-      },
+  const reserveDate = document.getElementById('today').textContent;
+
+  Promise.all([
+    fetch('/admin/reservation/roomList.do', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({})
     })
-    .then(response => response.json())
-    .then(data =>{
-      
-      
-      const checkInList = document.getElementById('list_checkIn');
-      checkInList.innerHTML = '';
-      let roomLists = data.roomList;
-      console.log(roomLists)
-      
-      roomLists.forEach(roomList =>{
-       if(roomList.depth != 0){
-        
-        let str = '<li class="List">';
-        str += '<p>객실명 :'+roomList.roomName+':/'+roomList.totalRoom+'</p>'
-          
-        checkInList.insertAdjacentHTML('beforeend', str); // 삽입
-       }
-       
-      })
-      
-    })
-  
-    /*
-    const reserveDate = document.getElementById('today').textContent;
-    console.log(typeof reserveDate);
-  
-    fetch('/admin/reservation/resList.do',{
-      method:'POST',
-      headers:{
-        'Content-Type' : 'application/json'
-      },
-      body:JSON.stringify({reservationDate : reserveDate})
-      
-    })
-    .then(response => response.json())
-    .then(data => {
-      
-    })
-    
-   */
-    
-    
-})
+      .then(response => response.json()),
 
-})
+    fetch('/admin/reservation/resList.do', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ reservationDate: reserveDate })
+    })
+      .then(response => response.json())
+  ]).then(([roomData, reservationData]) => {
+    const checkInList = document.getElementById('list_checkIn');
+    checkInList.innerHTML = '';
+
+    console.log(roomData, reservationData);
+
+    let reservationCounts = {};
+
+    // 예약 데이터 매핑
+    reservationData.reserves.forEach(res => {
+      const roomNo = res.roomNo;
+      reservationCounts[roomNo] = (reservationCounts[roomNo] || 0) + 1;
+    });
+
+    roomData.roomList.forEach(roomList => {
+      if (roomList.depth != 0) {
+        let reservationCount = reservationCounts[roomList.roomNo] || 0;
+
+        let str = `
+          <li class="List">
+            <p>객실명: ${roomList.roomName} (총 객실 수: ${roomList.totalRoom}) 
+            - 예약 수: ${reservationCount}</p>
+            <div class="reservation-details">
+              ${generateDetails(roomList.roomNo, reservationData.reserves)}
+            </div>
+          </li>`;
+
+        checkInList.insertAdjacentHTML('beforeend', str);
+      }
+    });
+
+    //예약상세
+    function generateDetails(roomNo, reserves) {
+      const roomReservations = reserves.filter(reservation => reservation.roomNo === roomNo);
+      
+      return roomReservations.map(reservation => `
+        <p>예약자: ${reservation.resName}, 예약 날짜: ${reservation.reservationDate}</p>
+      `).join('');
+    }
+
+  });
+
+});
